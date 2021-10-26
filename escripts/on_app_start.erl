34850ea12083b527bd640e8b2db464578f5e1e2f
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2021-10-26 14:27:00 +0800

    on_app_start

--- escripts/on_app_start.erl
+++ escripts/on_app_start.erl
@@ -8,22 +8,9 @@ main(_Args) ->
   interprete_modules().
 
 interprete_modules() ->
-  int:ni(sync),
-  int:ni(sync_scanner),
-  int:ni(sync_notify),
-  int:ni(sync_options),
-  int:ni(sync_utils),
-  int:ni(fs_app),
-  int:ni(inotifywait_win32),
-  int:ni(kqueue),
-  int:ni(fs_server),
-  int:ni(fs_event_bridge),
-  int:ni(fsevents),
-  int:ni(fs_sup),
-  int:ni(inotifywait),
-  int:ni(fs),
-  int:ni(timer_tests),
-  int:ni(gen_timer_sup),
-  int:ni(gen_timer),
+  % int:ni(demo_sup),
+  int:i([M || {M, _} <- code:all_loaded(), not code:is_sticky(M), int:interpretable(M) =:= true]),
 
+  % 这个会加载所有的系统库，很卡，慎用
+  % int:i([M || M <-[list_to_atom(M) || {M, _, _} <- code:all_available()], not code:is_sticky(M), int:interpretable(M) =:= true]),
   io:format("输入 int:interpreted(). 或者 il(). 查看模块列表~n").
